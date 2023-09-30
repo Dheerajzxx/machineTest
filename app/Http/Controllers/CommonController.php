0f4e632c184487a97bfad5f56e5f5275af406d86
Dheerajzxx <dheerajzxx@gmail.com>
2023-09-30 09:43:31 +0530

    bug fix

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -10,38 +10,23 @@ class CommonController extends Controller
     public function index(Request $request)
     {
       try{
-        $collection = array();
         $response = Http::get('https://opencontext.org/query/Asia/Turkey/Kenan+Tepe.json');
-        $res = $response->json();
-        foreach ($res as $key => $value) {
-          $data_type = gettype($value);
-          if($data_type == 'array'){
-            $new_value = $value;
-            while(isset($new_value[0]) && gettype($new_value[0]) != 'string' && array_key_exists(0, $new_value)){
-              $ids = array_column($new_value, 'id');
-              if(!empty($ids)){
-                foreach ($ids as $id) {
-                  array_push($collection, $id);
-                }
-              }
-              $new_value = $new_value[0];
-            }
-          }
-          else if($key == 'id')
-          array_push($collection, $value);
+        $res = $response->body();
+        preg_match_all('/\bid\":.*,/',$res, $matches);
+        foreach ($matches[0] as $key => $match) {
+          $matches[0][$key] = substr($match, 6, -2);
         }
         $new_collection = [];
-        foreach ($collection as $key => $value) {
+        foreach ($matches[0] as $key => $value) {
           if (!in_array($value, $new_collection)) {
             array_push($new_collection, $value);
           }
         }
-        
-
-        return view('test', compact('new_collection'));
+        return view('test', compact('new_collection', 'res'));
       }catch(\Exception $e){
         dd($e);
       }
     }
+  
 
 }
